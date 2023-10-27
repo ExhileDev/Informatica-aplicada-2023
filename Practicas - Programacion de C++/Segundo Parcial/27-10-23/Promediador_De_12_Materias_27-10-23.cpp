a52e33fff9d776d7941571dec92f917400041355
Exhile <107898268+ExhileDev@users.noreply.github.com>
2023-10-27 02:35:39 -0600

    Creamos un promediador como practica de informatica

---
+++ Practicas - Programacion de C++/Segundo Parcial/27-10-23/Promediador_De_12_Materias_27-10-23.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+main(){
+	string Materia[] = {"Matematicas: ","Fisica: ","Lengua Espanola: ","Geografia: ","Informatica: ","Historia: ","Educacion fisica: ","Lengua extranjera: ","Logica: ","Orientacion educactiva: ","Dibujo: ","Educacion Artistica: "};
+	float Calificacion[12];float resultado = 0;
+	for(int z=0; z<=11; z++){
+	cout<<"=============================Promediador de 12 materias============================\n";
+	cout<<"Dame la calificacion de "<<Materia[z];cin>>Calificacion[z];
+	resultado += Calificacion[z];
+	system("cls");
+	}
+	float promedio = resultado/12;
+	cout<<"=============================Promediador de 12 materias============================\n";
+	cout<<"El promediador calcula que tu promedio global es de "<<promedio;
+	return 321183613;
+}
