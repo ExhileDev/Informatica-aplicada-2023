b3127cbe6411a518e6281c7149f556aa43e1f833
Exhile <107898268+ExhileDev@users.noreply.github.com>
2023-10-27 02:42:56 -0600

    Agregamos un if al promediador para evitar poner calificaciones arriba de 10

--- Practicas - Programacion de C++/Segundo Parcial/27-10-23/Promediador_De_12_Materias_27-10-23.cpp
+++ Practicas - Programacion de C++/Segundo Parcial/27-10-23/Promediador_De_12_Materias_27-10-23.cpp
@@ -8,7 +8,12 @@ main(){
 	for(int z=0; z<=11; z++){
 	cout<<"=============================Promediador de 12 materias============================\n";
 	cout<<"Dame la calificacion de "<<Materia[z];cin>>Calificacion[z];
-	resultado += Calificacion[z];
+	if(Calificacion[z]>11){
+	cout<<"No puedes tener una calificacion mayor a 10, por defecto te pondremos 0";
+	Calificacion[z] = 0;
+	Sleep(1500);
+	}
+	resultado += Calificacion[z];	
 	system("cls");
 	}
 	float promedio = resultado/12;
