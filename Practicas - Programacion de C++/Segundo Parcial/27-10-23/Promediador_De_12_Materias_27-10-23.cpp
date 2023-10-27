b13829a249c77dfae8b393b487b6ac6972bdbc0d
Exhile <107898268+ExhileDev@users.noreply.github.com>
2023-10-27 03:07:58 -0600

    Fixeamos errores de ejecucion

--- Practicas - Programacion de C++/Segundo Parcial/27-10-23/Promediador_De_12_Materias_27-10-23.cpp
+++ Practicas - Programacion de C++/Segundo Parcial/27-10-23/Promediador_De_12_Materias_27-10-23.cpp
@@ -8,16 +8,13 @@ main(){
 	for(int z=0; z<=11; z++){
 	cout<<"=============================Promediador de 12 materias============================\n";
 	cout<<"Dame la calificacion de "<<Materia[z];cin>>Calificacion[z];
-	if(Calificacion[z]>11){
-	cout<<"No puedes tener una calificacion mayor a 10, por defecto te pondremos 0";
-	Calificacion[z] = 0;
-	Sleep(1500);
-	}
+	if(Calificacion[z]>11){cout<<"No puedes tener una calificacion mayor a 10, por defecto te pondremos 0";Calificacion[z] = 0;Sleep(1500);}
 	resultado += Calificacion[z];	
 	system("cls");
 	}
 	float promedio = resultado/12;
 	cout<<"=============================Promediador de 12 materias============================\n";
 	cout<<"El promediador calcula que tu promedio global es de "<<promedio;
+	Sleep(3000);
 	return 321183613;
 }
