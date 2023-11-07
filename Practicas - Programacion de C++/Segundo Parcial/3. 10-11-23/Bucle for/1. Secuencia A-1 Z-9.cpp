001251a37db3ae7fab866422a66733153c9a8252
Jorge Martinez laptop <jorgemartinezalvarez673@gmail.com>
2023-11-07 17:15:28 -0600

    Reorganizamos otra vez

--- Practicas - Programacion de C++/Segundo Parcial/3. 10-11-23/Bucle for/1. Secuencia A-1 Z-9.cpp
+++ Practicas - Programacion de C++/Segundo Parcial/3. 10-11-23/Bucle for/1. Secuencia A-1 Z-9.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 main(){
 	char letras[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','X','Y','Z'};
-	for(int i=0; i<=24; i++){
+	for(int i=0; i<=24 ; i++){
 		int e=i+1;
 		cout<<letras[i]<<" - "<<e<<endl;
 	}
